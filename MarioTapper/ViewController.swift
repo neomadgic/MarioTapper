3d0ae562e364f6c71cca7b189c6e0ad750ac51cd
Vu Dang <neomadgic@Vus-Mac.local>
2015-11-23 01:09:03 -0600

    Finished button functions. app complete

--- MarioTapper/ViewController.swift
+++ MarioTapper/ViewController.swift
@@ -10,24 +10,76 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    //Variables
+    var currentTaps = 0;
+    var maxTaps = 0;
     
+    //buttons
     @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var playBtn: UIButton!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var blockBtn: UIButton!
     @IBOutlet weak var numTapsLbl: UILabel!
     
+    
     @IBAction func playBtnPressed(sender: AnyObject)
     {
-        logo.hidden = true;
-        playBtn.hidden = true;
-        howManyTapsTxt.hidden = true;
-        blockBtn.hidden = false;
-        numTapsLbl.hidden = false;
+        if (howManyTapsTxt.text != nil && howManyTapsTxt.text != "")
+        {
+            logo.hidden = true;
+            playBtn.hidden = true;
+            howManyTapsTxt.hidden = true;
+            blockBtn.hidden = false;
+            numTapsLbl.hidden = false;
+            
+            currentTaps = 0;
+            maxTaps = Int(howManyTapsTxt.text!)!;
+            
+            updateTaps();
+            
+        }
+    }
+    
+    @IBAction func blockBtnPressed(sender: UIButton)
+    {
+        currentTaps++
+        updateTaps();
         
+        if isMaxTapsReached()
+        {
+            restartGame();
+        
+        }
+        
+    }
+    
+    func restartGame()
+    {
+        logo.hidden = false
+        playBtn.hidden = false;
+        howManyTapsTxt.hidden = false;
+        blockBtn.hidden = true;
+        numTapsLbl.hidden = true;
+        maxTaps = 0;
+        howManyTapsTxt.text = "";
         
     }
 
-
+    func updateTaps()
+    {
+        numTapsLbl.text = "\(currentTaps)/\(maxTaps) Taps"
+    }
+    
+    func isMaxTapsReached () -> Bool
+    {
+        if currentTaps >= maxTaps
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
 
